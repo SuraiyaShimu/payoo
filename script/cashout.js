0a86877940f64c24d90e560482ea9be284db4f43
SuraiyaShimu <shimu242-35-267@diu.edu.bd>
2026-05-13 20:25:24 +0600

    implement cashout functionality

--- script/cashout.js
+++ script/cashout.js
@@ -1,10 +1,50 @@
 document.getElementById("cashout-btn").addEventListener("click", function(){
+
     // 1- get the agent number & validation
-    // 2- get the amount, validation, convert to number
-    // 3- get the current balance, validation, convert to number
+        const cashoutNumberInput = document.getElementById("cashout-number");
+        const cashoutNumber = cashoutNumberInput.value;
+        console.log(cashoutNumber);
+        if(cashoutNumber.length !=11){
+            alert("Invalid Agent Number");
+            return;
+        }
+
+
+    // 2- get the amount
+        const cashoutAmountInput = document.getElementById("cashout-amount");
+        const cashoutAmount = cashoutAmountInput.value;
+        console.log(cashoutAmount);
+
+
+    // 3- get the current balance
+        const balanceElement = document.getElementById("balance");
+        const balance = balanceElement.innerText;
+        console.log(balance);
+
     // 4- calculate new balance
+        const newBalance = Number(balance) - Number(cashoutAmount);
+      
+        if(newBalance < 0){
+            alert("Invalid Amount");
+            return;
+        }
+       
+
     // 5-get the pin and verify
-    // 5.1- true:: show an alert > set balance
-    // 5.2- true:: show an error alert > return
+        const cashoutPinInput = document.getElementById("cashout-pin");
+        const pin = cashoutPinInput.value;
+        if(pin === '1234'){
+             // 5.1- true:: show an alert > set balance
+             alert("cashout successfull");
+              console.log("new balance", newBalance);
+              balanceElement.innerText = newBalance;
+        }else{
+            // 5.2- true:: show an error alert > return
+            alert("invalid pin");
+            return;
+        }
+
+   
+    
 
 });
