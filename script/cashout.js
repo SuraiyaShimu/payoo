957929be0076b828eb8ddf026b25d3f3a05cffd8
SuraiyaShimu <shimu242-35-267@diu.edu.bd>
2026-05-13 17:05:27 +0600

    design cashout form and logical implementation

---
+++ script/cashout.js
@@ -0,0 +1,10 @@
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    // 1- get the agent number & validation
+    // 2- get the amount, validation, convert to number
+    // 3- get the current balance, validation, convert to number
+    // 4- calculate new balance
+    // 5-get the pin and verify
+    // 5.1- true:: show an alert > set balance
+    // 5.2- true:: show an error alert > return
+
+});
